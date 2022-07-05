730cf0fb912d567488c821153103930494903461
DXTkastb <dxtkastb@gmail.com>
2022-07-05 14:30:11 +0530

    stable progress added

--- lib/main.dart
+++ lib/main.dart
@@ -107,6 +107,7 @@ class MainApp extends StatefulWidget {
 }
 
 class MainAppState extends State<MainApp> {
+  final GlobalKey<ScaffoldState> _key = GlobalKey<ScaffoldState>();
   late Future tasklist;
   late Future btasklist;
   late Future stat;
@@ -114,6 +115,7 @@ class MainAppState extends State<MainApp> {
   DateTime today = DateTime.now();
   late Timer tt;
   int _currentindex = 0;
+  bool loading = false;
 
   @override
   void initState() {
@@ -124,16 +126,20 @@ class MainAppState extends State<MainApp> {
     var tomorrow = today.add(const Duration(days: 1));
     Duration diff = DateTime(tomorrow.year, tomorrow.month, (tomorrow.day + 1))
         .difference(today);
-    print(diff.inSeconds);
-    tt = Timer(diff, () {
-      Future.delayed(Duration.zero, () async {
-        await DatabaseManager.databaseManagerInstance
-            .onNewDay(DatabaseManager.databaseManagerInstance.db);
-      }).then((value) {
-        setState(() {
-          setFutures();
-          today = today;
-        });
+    tt = Timer(diff, () async {
+      if (_key.currentState!.isDrawerOpen) {
+        Navigator.of(context).pop();
+      }
+      setState(() {
+        loading = true;
+      });
+      await DatabaseManager.databaseManagerInstance
+          .onNewDay(DatabaseManager.databaseManagerInstance.db);
+      await Future.delayed(const Duration(seconds: 3));
+      setState(() {
+        setFutures();
+        today = DateTime.now();
+        loading = false;
       });
     });
     super.initState();
@@ -164,121 +170,150 @@ class MainAppState extends State<MainApp> {
   Widget build(BuildContext context) {
     // upFutures();
 
-    return Scaffold(
-      drawer: SafeArea(
-        child: Drawer(
-          child: CustomDrawerColumn(height),
-        ),
-      ),
-      body: LayoutBuilder(
-        builder: (ctx, cons) {
-          return Theme(
-              data: ThemeData(
-                  textTheme: TextTheme(
-                headline1: TextStyle(
-                    fontSize: cons.maxHeight / 25,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black.withOpacity(1)),
-                headline2: TextStyle(
-                    fontSize: cons.maxHeight / 26,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black),
-                headline3: TextStyle(
-                    fontSize: cons.maxHeight / 32,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black.withOpacity(0.6)),
-                headline4: TextStyle(
-                  color: Colors.deepPurple,
-                  fontSize: cons.maxHeight / 28,
-                  fontWeight: FontWeight.bold,
-                ),
-                headline6: TextStyle(
-                    fontSize: cons.maxHeight / 28,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black),
-              )),
-              child: (_currentindex == 1)
-                  ? BriefTaskPage(
-                      btasklist,
-                    )
-                  : TasksPage(
-                      tasklist,
-                    ));
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if (_currentindex == 0) {
-            removeAnyScaffoldSnack();
-            Navigator.of(context).pushNamed('/addtask').then((value) {
-              if (value == true) {
-                ScaffoldMessenger.of(context)
-                    .showSnackBar(const SnackBar(content: Text('Task Added!')));
-              }
-            });
-          } else {
-            removeAnyScaffoldSnack();
-            Navigator.of(context).pushNamed('/addbtask').then((value) {
-              if (value == true) {
-                ScaffoldMessenger.of(context)
-                    .showSnackBar(const SnackBar(content: Text('Task Added!')));
-              }
-            });
-          }
-        },
-        backgroundColor: (_currentindex == 1) ? Colors.teal : Colors.deepPurple,
-        child: const Icon(
-          Icons.add_box_rounded,
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.shifting,
-        currentIndex: _currentindex,
-        onTap: (x) {
-          removeAnyScaffoldSnack();
-          if (_currentindex != x) {
-            setState(() {
-              _currentindex = x;
-            });
-          }
-        },
-        items: [
-          BottomNavigationBarItem(
-              backgroundColor: Colors.deepPurple,
-              icon: Icon(
-                Icons.alarm_rounded,
-                size: height / 27,
+    return (loading)
+        ? Scaffold(
+            backgroundColor: Colors.deepOrange,
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  CircularProgressIndicator(
+                    color: Colors.white,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Text(
+                      'SYNCING DATA',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15),
+                    ),
+                  )
+                ],
               ),
-              label: 'Daily Tasks'),
-          BottomNavigationBarItem(
-              backgroundColor: Colors.teal,
-              icon: Icon(
-                Icons.task_rounded,
-                size: height / 27,
-              ),
-              label: 'Brief Tasks'),
-        ],
-      ),
-      appBar: AppBar(
-        toolbarHeight: height / 13,
-        shape: const ContinuousRectangleBorder(
-            borderRadius: BorderRadius.only(
-                bottomRight: Radius.circular(30),
-                bottomLeft: Radius.circular(30))),
-        backgroundColor: (_currentindex == 0) ? Colors.deepPurple : Colors.teal,
-        title: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Resk',
-              style:
-                  TextStyle(fontSize: height / 28, fontWeight: FontWeight.bold),
             ),
-            const Expanded(child: SizedBox()),
-            (_currentindex == 0) ? CircleIndicator(stat) : const SizedBox(),
-          ],
-        ),
-      ),
-    );
+          )
+        : Scaffold(
+            key: _key,
+            drawer: SafeArea(
+              child: Drawer(
+                child: CustomDrawerColumn(height),
+              ),
+            ),
+            body: LayoutBuilder(
+              builder: (ctx, cons) {
+                return Theme(
+                    data: ThemeData(
+                        textTheme: TextTheme(
+                      headline1: TextStyle(
+                          fontSize: cons.maxHeight / 25,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black.withOpacity(1)),
+                      headline2: TextStyle(
+                          fontSize: cons.maxHeight / 26,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                      headline3: TextStyle(
+                          fontSize: cons.maxHeight / 32,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black.withOpacity(0.6)),
+                      headline4: TextStyle(
+                        color: Colors.deepPurple,
+                        fontSize: cons.maxHeight / 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      headline6: TextStyle(
+                          fontSize: cons.maxHeight / 28,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    )),
+                    child: (_currentindex == 1)
+                        ? BriefTaskPage(
+                            btasklist,
+                          )
+                        : TasksPage(
+                            tasklist,
+                          ));
+              },
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                if (_currentindex == 0) {
+                  removeAnyScaffoldSnack();
+                  Navigator.of(context).pushNamed('/addtask').then((value) {
+                    if (value == true) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Task Added!')));
+                    }
+                  });
+                } else {
+                  removeAnyScaffoldSnack();
+                  Navigator.of(context).pushNamed('/addbtask').then((value) {
+                    if (value == true) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Task Added!')));
+                    }
+                  });
+                }
+              },
+              backgroundColor:
+                  (_currentindex == 1) ? Colors.teal : Colors.deepPurple,
+              child: const Icon(
+                Icons.add_box_rounded,
+              ),
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+              type: BottomNavigationBarType.shifting,
+              currentIndex: _currentindex,
+              onTap: (x) {
+                removeAnyScaffoldSnack();
+                if (_currentindex != x) {
+                  setState(() {
+                    _currentindex = x;
+                  });
+                }
+              },
+              items: [
+                BottomNavigationBarItem(
+                    backgroundColor: Colors.deepPurple,
+                    icon: Icon(
+                      Icons.alarm_rounded,
+                      size: height / 27,
+                    ),
+                    label: 'Daily Tasks'),
+                BottomNavigationBarItem(
+                    backgroundColor: Colors.teal,
+                    icon: Icon(
+                      Icons.task_rounded,
+                      size: height / 27,
+                    ),
+                    label: 'Brief Tasks'),
+              ],
+            ),
+            appBar: AppBar(
+              toolbarHeight: height / 13,
+              shape: const ContinuousRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(30),
+                      bottomLeft: Radius.circular(30))),
+              backgroundColor:
+                  (_currentindex == 0) ? Colors.deepPurple : Colors.teal,
+              title: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Resk',
+                    style: TextStyle(
+                        fontSize: height / 28, fontWeight: FontWeight.bold),
+                  ),
+                  const Expanded(child: SizedBox()),
+                  (_currentindex == 0)
+                      ? CircleIndicator(stat)
+                      : const SizedBox(),
+                ],
+              ),
+            ),
+          );
   }
 }
